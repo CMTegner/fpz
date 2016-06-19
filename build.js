8f36ac5b5d4d21f112a661c98400ee69b7504e8f
Christian Tegnér <christian.tegner@gmail.com>
2016-06-19 21:02:01 +0200

    Remove some development code

--- build.js
+++
@@ -1,7 +0,0 @@
-var browserify = require("browserify");
-var fs = require("fs");
-
-var modules = browserify("./test.js");
-
-var output = fs.createWriteStream("bundle.js", { encoding: "UTF-8" });
-modules.bundle().pipe(output);
